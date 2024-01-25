9c477685ca69c5f3eca259a95734944dcf059665
Roman Rios <romanrios@live.com>
2024-01-25 13:12:21 -0300

    first commit

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1706198611,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg_-sheet0.png",
+		"images/body_-sheet0.png",
+		"images/face2removebgpreview-sheet0.png",
+		"images/01_left_eye-sheet0.png",
+		"images/01_mouth-sheet0.png",
+		"images/fullscreen-sheet0.png",
+		"images/fullscreen2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
